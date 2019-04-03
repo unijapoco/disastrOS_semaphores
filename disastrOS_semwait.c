228a367f60de1eb835e365ee09b066ee525a29a0
unijapoco <jacopo@airmail.cc>
2019-04-03 18:36:26 +0200

    initial sempost implementation

--- disastrOS_semwait.c
+++ disastrOS_semwait.c
@@ -7,5 +7,36 @@
 #include "disastrOS_semdescriptor.h"
 
 void internal_semWait(){
-  // do stuff :)
+	int semnum = running->syscall_args[0];
+
+	Semaphore *sem = SemaphoreList_byId(&semaphores_list, semnum);
+	if (!sem) {
+		running->syscall_retvalue = DSOS_ENOEX;
+		return;
+	}
+
+	SemDescriptorPtr *desptr = SemDescriptorList_byPID(&sem->descriptors, running->pid);
+	if (!sem) {
+		running->syscall_retvalue = DSOS_ENOPER;
+		return;
+	}
+
+	if (sem->count > 0) {
+		sem->count--;
+		running->syscall_retvalue = 0;
+		return;
+	}
+
+	List_insert(&sem->waiting_descriptors, sem->waiting_descriptors.last, (Listitem *)desptr);
+	running->status = Waiting;
+	List_insert(&waiting_list, waiting_list.last, (ListItem *)running);
+	if (ready_list.first) {
+		running = (PCB *)List_detach(&ready_list, ready_list.first);
+	} else {
+		running = 0;
+		printf("Everyone's asleep\n");
+		disastrOS_printStatus();
+	}
+	running->syscall_retvalue = 0;
+	return;
 }
