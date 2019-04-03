6e35ba51123df99f0eff847eacfe364df8db4ccd
unijapoco <jacopo@airmail.cc>
2019-04-03 18:37:04 +0200

    initial sempost implementation

--- disastrOS_sempost.c
+++ disastrOS_sempost.c
@@ -7,5 +7,32 @@
 #include "disastrOS_semdescriptor.h"
 
 void internal_semPost(){
-  // do stuff :)
+	int semnum = running->syscall_args[0];
+
+	Semaphore *sem = SemaphoreList_byId(&semaphores_list, semnum);
+	if (!sem) {
+		running->syscall_retvalue = DSOS_ENOEX;
+		return;
+	}
+
+	SemDescriptorPtr *desptr = SemDescriptorList_byPID(&sem->descriptors, running->pid);
+	if (!sem) {
+		running->syscall_retvalue = DSOS_ENOPER;
+		return;
+	}
+
+	sem->count++;
+	if (sem->count == (0 + 1) && sem->waiting_descriptors.first) {
+		List_detach(&sem->waiting_descriptors, (ListItem *)desptr);
+		next_proc = desptr->descriptor->pcb;
+		List_detach(&waiting_list, next_proc);
+
+		running->status = Ready;
+		List_insert(&ready_list, ready_list.last, (ListItem *)running);
+
+		next_proc->status = Running;
+		running = next_proc;
+	}
+	running->syscall_retvalue = 0;
+	return;
 }
