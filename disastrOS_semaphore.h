dc8c5decd31e597731121368522323e26dfa27f9
unijapoco <jacopo@airmail.cc>
2019-04-03 17:31:35 +0200

    implement semclose

--- disastrOS_semaphore.h
+++ disastrOS_semaphore.h
@@ -4,7 +4,9 @@
 
 #define DSOS_ENOMEM -0xc1400000
 #define DSOS_EINVAL -0xc1400001
-#define DSOS_ESEMNOFD -0xc140002
+#define DSOS_ESEMNOFD -0xc1400002
+#define DSOS_ENOEX -0xc1400003
+#define DSOS_ENOPER -0xc1400004
 
 typedef struct {
   ListItem list;
--- disastrOS_semclose.c
+++ disastrOS_semclose.c
@@ -7,5 +7,32 @@
 #include "disastrOS_semdescriptor.h"
 
 void internal_semClose(){
-  // do stuff :)
+	int semnum = running->syscall_args[0];
+
+	Semaphore *sem = SemaphoreList_byId(&semaphores_list, semnum);
+	if (!sem) {
+		running->syscall_retvalue = DSOS_ENOEX;
+		return;
+	}
+
+	SemDescriptorPtr *desptr = SemDescriptorList_byPID(&sem->descriptors, running->pid);
+	if (!sem) {
+		running->syscall_retvalue = DSOS_ENOPER;
+		return;
+	}
+
+	SemDescriptor *des = ListDetach(&running->sem_descriptors, (ListItem *)desptr->descriptor);
+
+	desptr = (SemDescriptorPtr *)List_detach(&sem->descriptors, (ListItem *)desptr);
+	SemDescriptor_free(des)
+	SemDescriptorPtr_free(desptr)
+
+	//if the semaphores has no more descriptors, i.e. it's not open in any process we destroy it
+	if (!sem->descriptors->size) {
+		sem = (Semaphore *)List_detach(&semaphores_list, (ListItem *)sem);
+		Semaphore_free(sem);
+	}
+
+	running->syscall_retvalue = 0;
+	return;
 }
