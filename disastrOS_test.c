7306e56c21153a6828884d3150177a4fdc4ada58
unijapoco <jacopo@airmail.cc>
2019-04-09 19:34:24 +0200

    created my own test

--- disastrOS_test.c
+++ disastrOS_test.c
@@ -14,67 +14,105 @@ void sleeperFunction(void* args){
   }
 }
 
-void childFunction(void* args){
-  printf("Hello, I am the child function %d\n",disastrOS_getpid());
-  printf("I will iterate a bit, before terminating\n");
-  int type=0;
-  int mode=0;
-  int fd=disastrOS_openResource(disastrOS_getpid(),type,mode);
-  printf("fd=%d\n", fd);
-  printf("PID: %d, terminating\n", disastrOS_getpid());
+void sem_test1(void *args) {
+	printf("1: Testing the various semaphore features\n");
+	printf("1: Creating a new semaphore with id: 1234\n");
+	int semnum = disastrOS_semOpen(1234);
+	printf("1: Return value: %d\n", semnum);
+  disastrOS_printStatus();
+	disastrOS_preempt();
 
-  for (int i=0; i<(disastrOS_getpid()+1); ++i){
-    printf("PID: %d, iterate %d\n", disastrOS_getpid(), i);
-    disastrOS_sleep((20-disastrOS_getpid())*5);
-  }
+	printf("1: Trying to create a semaphore with a negative id\n");
+	int error_semnum = disastrOS_semOpen(-1234);
+  disastrOS_printStatus();
+	printf("1: Function returned error value: %d\n", error_semnum);
+
+	printf("1: Closing the semaphore 1234\n");
+	int res = disastrOS_semClose(1234);
+	printf("1: Return value: %d\n", res);
+  disastrOS_printStatus();
+	disastrOS_preempt();
+
+	printf("1: Creating a new semaphore with id: 4321\n");
+	semnum = disastrOS_semOpen(4321);
+  disastrOS_printStatus();
+	disastrOS_preempt();
+
+	printf("1: Simulating a critical section\n");
+	res = disastrOS_semWait(4321);
+	printf("1: INSIDE THE CRITICAL SECTION\n");
+  disastrOS_printStatus();
+	printf("1: going to sleep\n");
+	disastrOS_sleep(10);
+	printf("1: FINISHING CRITICAL SECTION\n");
+  disastrOS_printStatus();
+	res = disastrOS_semPost(4321);
+	printf("1: semPost return value: %d\n", res);
+  disastrOS_printStatus();
+	printf("1: Cleaning up now\n");
+	res = disastrOS_semClose(4321);
+	printf("1: semClose return value %d", res);
+  disastrOS_printStatus();
+
   disastrOS_exit(disastrOS_getpid()+1);
 }
 
-
-void initFunction(void* args) {
+void sem_test2(void *args) {
+	printf("2: Opening the semaphore my brother created\n");
+	int semnum = disastrOS_semOpen(1234);
+	printf("2: Return value: %d\n", semnum);
   disastrOS_printStatus();
-  printf("hello, I am init and I just started\n");
-  disastrOS_spawn(sleeperFunction, 0);
-
-
-  printf("I feel like to spawn 10 nice threads\n");
-  int alive_children=0;
-  for (int i=0; i<10; ++i) {
-    int type=0;
-    int mode=DSOS_CREATE;
-    printf("mode: %d\n", mode);
-    printf("opening resource (and creating if necessary)\n");
-    int fd=disastrOS_openResource(i,type,mode);
-    printf("fd=%d\n", fd);
-    disastrOS_spawn(childFunction, 0);
-    alive_children++;
-  }
-
   disastrOS_printStatus();
-  int retval;
-  int pid;
-  while(alive_children>0 && (pid=disastrOS_wait(0, &retval))>=0){
-    disastrOS_printStatus();
-    printf("initFunction, child: %d terminated, retval:%d, alive: %d \n",
-	   pid, retval, alive_children);
-    --alive_children;
-  }
-  printf("shutdown!");
-  disastrOS_shutdown();
-}
+	disastrOS_preempt();
 
-void sem_test1(void *args) {
-	int semnum = disastrOS_semOpen(0);
+	printf("2: Closing the semaphore 1234 (that my brother created)\n");
+	int res = disastrOS_semClose(1234);
+	printf("2: Return value: %d\n", res);
   disastrOS_printStatus();
-	disastrOS_semClose(semnum);
+	printf("2: Notice the semaphore was also destroyed, because it had no more open descriptors\n");
+	disastrOS_preempt();
+
+	printf("2: Trying to close a semaphore I don't own\n");
+	res = disastrOS_semClose(4321);
+	printf("2: Return value: %d\n", res);
   disastrOS_printStatus();
+
+	printf("2: Trying to wait on a semaphore I don't own\n");
+	res = disastrOS_semWait(4321);
+	printf("2: Return value: %d\n", res);
+  disastrOS_printStatus();
+
+	printf("2: Trying to post a semaphore I don't own\n");
+	res = disastrOS_semPost(4321);
+	printf("2: Return value: %d\n", res);
+  disastrOS_printStatus();
+
+	semnum = disastrOS_semOpen(4321);
+  disastrOS_printStatus();
+
+	disastrOS_preempt();
+
+	printf("2: Waiting on my brother's semaphore, you won't hear back from me until my brother posts it\n");
+	res = disastrOS_semWait(4321);
+	printf("2: semwait return value: %d\n", res);
+	printf("2: Entering my critical section immediately\n");
+	res = disastrOS_semPost(4321);
+
+	res = disastrOS_semClose(4321);
+	printf("2: semclose return value: %d\n", res);
+  disastrOS_printStatus();
+	disastrOS_preempt();
+
   disastrOS_exit(disastrOS_getpid()+1);
 }
 
 void my_init(void *args) {
+  disastrOS_spawn(sleeperFunction, 0);
 	disastrOS_printStatus();
 	disastrOS_spawn(sem_test1, 0);
+	disastrOS_spawn(sem_test2, 0);
 	int retval;
+	disastrOS_wait(0, &retval);
 	disastrOS_wait(0, &retval);
 	disastrOS_shutdown();
 }
